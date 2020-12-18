82878c85a7b24dd589e0dc698b2ca5f25ef41472
tye <joetiehu2006@gmail.com>
2020-12-18 11:10:41 -0500

    add socket in pyLight.py

--- pyLight.py
+++ pyLight.py
@@ -1,9 +1,27 @@
+#!/usr/bin/env python3
 '''
 Author: Tie Hu
 Date: 2018/08/18
 Description: Code to control led light on the raspberry
 '''
-import numpy as np
+
+import socket
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
+
 
 
 
